dd3c2f2ac36be1560f9355f01932b47a4d7e7c02
aflock <andrewbflockhart@gmail.com>
2011-12-23 23:27:26 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T3kno::Application.config.secret_token = '3d4f518c3302324e684dbf1c5fa40f597e9b42039cc598f28c1556a4ef059a4482f97d680d5f5b760b30647b73267f9ebab46857ae933c495e00070035d64f1e'
